6a2448b2aaa88409ecd55f090fbf54c917c6768e
Devender Gupta <devender.gupta@accionlabs.com>
2016-03-04 15:01:47 +0530

    Added employee data and css

--- app/js/controllers/helloWorldController.js
+++ app/js/controllers/helloWorldController.js
@@ -1,4 +1,19 @@
 var app = angular.module('app',[]);
 app.controller('HelloWorldCntrl', ['$scope', function($scope){
 	$scope.testMessage = "Hello World!!!";
+
+	$scope.employees = [
+		{
+			"name": "John Clark",
+			"designation": "I. T. Manager"
+		},
+		{
+			"name": "Gracy C. Angle",
+			"designation": "Sales Executive"
+		},
+		{
+			"name": "Mike Preria",
+			"designation": "Marketing Head"
+		}
+	]
 }])
