d0c0a2f729eae5f02d4998a7816ce9ddd3152488
Amit Kumar <amit.kumar@gestotech.com>
2016-06-27 15:13:18 +0530

    added uuid tooltip

--- app/js/components/RootComponent/LinkComponent.jsx
+++ app/js/components/RootComponent/LinkComponent.jsx
@@ -1,15 +1,21 @@
 import React from 'react';
+import ReactTooltip from 'react-tooltip'
 
 class LinkComponent extends React.Component
 {
   constructor(props){
     super(props);
     this.state = {
-      data: props.data
+      data: props.data,
+      iuuid: props.rowData.iuuid
     };
   }
   render(){
-    return <a href="#">{this.state.data}</a>;
+    return <div>
+              <a href="#" data-tip={this.state.iuuid}>{this.state.data}</a>
+              <ReactTooltip place="top" type="dark" effect="float"/>
+            </div>;
+
   }
 }
 
